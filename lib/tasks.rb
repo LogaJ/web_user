5d2e5d345ce8f88902801e5edcd88bc1b453ae3e
Despo Pentara <despo@extractmethod.com>
2011-08-25 12:49:30 +0100

    moving tasks to their own directory
    easier to identify

---
+++ lib/tasks.rb
@@ -0,0 +1,7 @@
+$:.unshift(File.join(File.dirname(__FILE__), "tasks"))
+
+require  'browser_startup'
+require  'login'
+require  'see_the_message'
+require  'see_the_user_status'
+require  'see_the_warning'

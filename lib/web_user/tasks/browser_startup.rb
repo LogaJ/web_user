cfe69bbe4c7d33076008aa4749d47ba55a93c1bd
Despo Pentara <despo@extractmethod.com>
2011-08-30 13:09:41 +0100

    Browser can be configured by environment

--- lib/web_user/tasks/browser_startup.rb
+++ lib/web_user/tasks/browser_startup.rb
@@ -6,16 +6,16 @@ module BrowserStartup
       settings = Selenium::WebDriver::Firefox::Profile.new
       settings["network.proxy.type"] = 1
       settings["network.proxy.no_proxies_on"] = "localhost, 127.0.0.1, *.dave"
-      driver = Selenium::WebDriver.for :firefox, :profile => settings
+      driver = Selenium::WebDriver.for browser, :profile => settings
     elsif OS.is_windows?
-      driver = :ie
+      driver = browser
     end
 
     @browser = Watir::Browser.new driver
   end
 
   def browser
-    return ENV['BROWSER'] if !ENV['BROWSER']
+    return ENV['RACK_ENV'].to_sym if ENV['RACK_ENV']
     return DEFAULT_BROWSER
   end
 

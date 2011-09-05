8a94b8cb1e54ba283806b5f02df13999214c987d
Despo Pentara <despo@despo.me>
2011-09-05 16:49:29 +0100

    improving task implementation for seeing user status

--- lib/web_user/tasks/see_in_the_user_status.rb
+++ lib/web_user/tasks/see_in_the_user_status.rb
@@ -1,5 +1,5 @@
 in_order_to 'see in the user status', content: :logged_in_text, and_username: :username do
   text = whats_the :user_status, :div, :text
-  text.include?(the :logged_in_text)
-  text.include?(the :username)
+  raise "The content: `#{the :logged_in_text}` is incorrect" unless text.include?(the :logged_in_text)
+  raise "The username: `#{the :user_name}` is incorrect" unless text.include?(the :username)
 end

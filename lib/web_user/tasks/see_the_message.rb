e804f08ba068a6a18d6cba42bf1bc5e1ddf5fdbe
Despo Pentara <despo+despo@extractmethod.com>
2011-09-06 16:51:32 +0100

    trying out watir wait until element loads...

--- lib/web_user/tasks/see_the_message.rb
+++ lib/web_user/tasks/see_the_message.rb
@@ -1,4 +1,9 @@
 in_order_to 'See The Message', the: :text do
+
+  Watir::Waiter::wait_until do
+    @browser.div(:class, /error_msg|display_msg|info/).exists?
+  end
+
   whats_the :feedback, :paragraph, :text
 end
 

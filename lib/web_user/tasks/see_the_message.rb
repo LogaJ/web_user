5d5f6ffcfb4441f69e8c5d02622f325858e7836c
Despo Pentara <despo+despo@extractmethod.com>
2011-09-07 09:16:45 +0100

    removing wait_until element loads
    this needs to be applied on click

--- lib/web_user/tasks/see_the_message.rb
+++ lib/web_user/tasks/see_the_message.rb
@@ -1,9 +1,5 @@
 in_order_to 'See The Message', the: :text do
 
-  Watir::Waiter::wait_until do
-    @browser.div(:class, /error_msg|display_msg|info/).exists?
-  end
-
   whats_the :feedback, :paragraph, :text
 end
 
